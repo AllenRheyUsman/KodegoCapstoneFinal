84cebee1ad7d0253979a61e667338cd1677b1706
Allen <arusman1987@gmail.com>
2023-06-04 14:18:17 +0800

    cleaned the folders

--- capstone_tester/hotel_tester/src/component/Admin/walkin.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Walkin() {
-  return (
-    <div>W</div>
-  )
-}
--- capstone_tester/hotel_tester/src/component/Header/navicon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Navicon() {
-  return (
-    <div className=' mx-0 px-0'>
-        
-    </div>
-  )
-}
